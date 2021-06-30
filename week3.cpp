af3b5751ba8aed0695fbd8e1c93a0092db82f756
James Nolan <vip17jamesnolan@gmail.com>
2021-06-30 09:20:51 +0800

    week 3

--- week3.cpp
+++ week3.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
+
 using namespace std;
 
-class Inline_Class{
+class Inline_Class {
     //These 2 are all inline func
     inline void func1();
-    void func2(){};
+
+    void func2() {};
 };
 
-void Inline_Class::func1(){};
 
-class Location{
+void Inline_Class::func1() {};
+
+class Location {
 private:
-    int x,y;
+    int x, y;
+public:
+    void init(int x = 0, int y = 0);
+
+    void valueX(int val) { x = val };
+
+    int valueX() { return x; }
+    //avoid 2 meanings
+    //void valueX(int val=0){x = val};
+    //int valueX(){return x;}
 };
+
+class Complex {
+private:
+    double real, imag;
+public:
+    Complex(double real, double imag = 0);
+};
+
+Complex::Complex(double real, double imag) {
+    this->real = real;
+    this->imag = imag;
+}
