98a93372718677f134c45650341f67fb7912239e
James Nolan <vip17jamesnolan@gmail.com>
2021-07-20 08:41:53 +0800

    wee4 4-1-2-1

--- week4.cpp
+++ week4.cpp
@@ -22,7 +22,30 @@ Complex operator+(const Complex &a, const Complex &b){
     return Complex(a.real + b.real, a.imag + b.imag);
 }
 
+class String{
+private:
+    char *str;
+public:
+    String():str(NULL){}
+    const char *c_str(){return str;};
+    char *operator=(const char* s);
+    ~String();
+};
 
+char *String:: operator=(const char * s){
+    if(str) delete[] str;
+    if (s){ //s != NULL
+        str = new char[strlen(s)+1];
+        strcpy(str,s);
+    }
+    else {
+        str = NULL;
+    }
+}
+
+String::~String(){
+    if (str) delete[] str;
+}
 
 int main(){
     Complex a(1,2), b(2,3), c, d;
@@ -30,4 +53,11 @@ int main(){
     d = a - b;
     cout << c.real << c.imag << endl;
     cout << d.real << d.imag << endl;
+
+    String s;
+    s = "Good Luck";
+    cout << s.c_str() << endl;
+    // String s2 = "hello";  this line is not corrrect because we don't have that constructor yet.
+
+    return 0;
 }
